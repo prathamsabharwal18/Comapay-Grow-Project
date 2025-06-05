f0b4db9a843450207d1fc0761c19bf95e5e4114a
prathamsabharwal18 <p.sabharwal@iitg.ac.in>
2025-06-05 15:20:45 +0530

    4th commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import projectRoutes from './routes/projectRoutes.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
@@ -26,7 +26,7 @@ app.use('/api/projects', projectRoutes);
 
 // MongoDB connection
 mongoose
-  .connect('mongodb://localhost:27017/companygrow', {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
